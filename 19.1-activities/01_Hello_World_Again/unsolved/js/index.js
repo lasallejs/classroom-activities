bfd84fd9369c96ce76a14de49214438dd58de446
lasallejs <janisa_lasalle@yahoo.com>
2021-09-22 19:03:29 -0400

    19.2

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,65 @@
 // Create a variable below
 
+var myFirstVariable ='This is my first variable';
+
+var janisa='Hello World, I am Janisa';
+
+
+
+
+
 // Console.log() your variable here.
+
+
+console.log(myFirstVariable);
+
+console.log(janisa);
+
+
+
+var redhat=5;
+var bluehat=9;
+
+
+// var total= 'Your total is' + (redhat + bluehat);
+
+var total= redhat + bluehat;
+
+
+alert(total);
+
+
+
+
+function addNumbers() {
+    var two=2;
+    var three=3;
+    var five = two + three;
+
+    console.log(five);
+
+}
+
+
+function addNumbers() {
+    var two='janisa';
+    var three='michaela';
+    var five = 'our teacher are ' + (two + three) ;
+   
+    console.log(five);
+
+}
+
+
+
+
+addNumbers(); 
+
+
+
+
+addNumbers(); 
+
+
+
+
